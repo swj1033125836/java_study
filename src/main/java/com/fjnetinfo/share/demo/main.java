6a42950f52691b4fb8ebb02a3371e6b5c2d2dbc8
施文剑 <a1033125836@gmail.com>
2019-12-20 20:40:51 +0800

    添加main

---
+++ src/main/java/com/fjnetinfo/share/demo/main.java
@@ -0,0 +1,9 @@
+package com.fjnetinfo.share.demo;
+
+public class main {
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+        Utils.parseData();
+    }
+}
